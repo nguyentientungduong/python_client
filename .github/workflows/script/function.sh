1644247a0f1e867822922ec27ad4ec5af960b9ab
Dang Trung Tin <tin1.dangtrung@toshiba.co.jp>
2021-02-17 10:20:46 +0700

    update

---
+++ .github/workflows/script/function.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+# Declare constants for OS Ubuntu, CentOS, openSUSE
+readonly UBUNTU=Ubuntu
+readonly CENTOS=Centos
+readonly OPENSUSE=Opensuse
+
+# Check if the file exists with the parameter path passed
+check_file_exist() {
+    local file_path=$1
+    if [ ! -f "$file_path" ]; then
+        echo "$file_path not found!"
+    fi
+}
+
+# Build Python Client
+build_python_client() {
+    # Update include file to python.h
+    make
+}
