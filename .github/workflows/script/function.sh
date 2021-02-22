6d18202839ab2f3468d43874fd42206a3c6a836a
Dang Trung Tin <tin1.dangtrung@toshiba.co.jp>
2021-02-22 16:39:15 +1000

    update123

--- .github/workflows/script/function.sh
+++ .github/workflows/script/function.sh
@@ -22,7 +22,8 @@ build_package() {
 
 # Check information rpm and deb package
 check_package() {
-    cd python_client/
+    pwd
+    ls
     version=$(cat setup.py | grep "version=" | cut -f 2 -d"'")
     package_path=dist/griddb_python-$version-cp36-cp36m-$WHLSUFFIX.whl
     check_file_exist "$package_path"
