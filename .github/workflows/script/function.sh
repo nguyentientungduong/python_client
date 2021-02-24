3e86b00bc7f0b0303ae75b067d5bee4fc20b4ecf
Dang Trung Tin <tin1.dangtrung@toshiba.co.jp>
2021-02-24 18:49:53 +1000

    update12345

--- .github/workflows/script/function.sh
+++ .github/workflows/script/function.sh
@@ -108,6 +108,7 @@ install_packages_macos() {
     brew install docker docker-machine virtualbox
     brew cleanup
     #docker-machine create default
+    mkdir -p ~/.docker/machine/cache/
     curl -Lo ~/.docker/machine/cache/boot2docker.iso https://github.com/boot2docker/boot2docker/releases/download/v18.09.1-rc1/boot2docker.iso
     docker-machine create --driver virtualbox --virtualbox-boot2docker-url ~/.docker/machine/cache/boot2docker.iso default
     rm ~/.docker/machine/cache/boot2docker.iso
