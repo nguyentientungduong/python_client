ea1dbcb1c80579360c1999691a04f85ab830a802
Dang Trung Tin <tin1.dangtrung@toshiba.co.jp>
2021-03-02 15:48:29 +0700

    message123

--- setup.py
+++ setup.py
@@ -243,8 +243,7 @@ if platform.system() == 'Darwin':
               ),
               [
                   f"/usr/local/Cellar/griddb-c-client/{cclient_version}/lib/libgridstore.0.dylib",  
-                  f"/usr/local/Cellar/griddb-c-client/{cclient_version}/lib/libgridstore.a"),
-                  f"/usr/local/Cellar/griddb-c-client/{cclient_version}/lib/libgridstore.dylib"
+                  f"/usr/local/Cellar/griddb-c-client/{cclient_version}/lib/libgridstore.a",
                   f"/usr/local/Cellar/griddb-c-client/{cclient_version}/lib/libgridstore.dylib"
 
               ],
