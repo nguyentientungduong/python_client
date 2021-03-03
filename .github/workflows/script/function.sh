ffccc2f1d16856d7b3e5e98e1a303070d9a0e575
Dang Trung Tin <tin1.dangtrung@toshiba.co.jp>
2021-03-03 10:00:20 +0700

    update1234456

--- .github/workflows/script/function.sh
+++ .github/workflows/script/function.sh
@@ -115,6 +115,7 @@ install_packages_macos() {
     wget https://github.com/griddb/c_client/archive/v$C_CLIENT_VERSION.tar.gz
     tar xvfz v$C_CLIENT_VERSION.tar.gz
     rm v$C_CLIENT_VERSION.tar.gz
+    ls
     cd c_client-$C_CLIENT_VERSION/c
     ./bootstrap.sh
     ./configure
