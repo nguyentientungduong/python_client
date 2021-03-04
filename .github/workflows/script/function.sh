3308fbc0590711fd20357477b0cd20432d9633b2
Dang Trung Tin <tin1.dangtrung@toshiba.co.jp>
2021-03-04 14:48:29 +0700

    update12345

--- .github/workflows/script/function.sh
+++ .github/workflows/script/function.sh
@@ -127,7 +127,8 @@ install_packages_macos() {
 build_package_macos() {
     source ~/.bash_profile
     ls -lah ./c_client-$C_CLIENT_VERSION/bin
-    export DYLD_LIBRARY_PATH=${DYLD_LIBRARY_PATH}:./c_client-$C_CLIENT_VERSION/bin/
+    export LIBRARY_PATH=$LIBRARY_PATH:./c_client-$C_CLIENT_VERSION/bin/
+    export DYLD_LIBRARY_PATH=$DYLD_LIBRARY_PATH:./c_client-$C_CLIENT_VERSION/bin/
     python setup.py bdist_wheel
     # Remove C Client in system: WHL package will include C Client
     cd c_client-$C_CLIENT_VERSION/client/c
