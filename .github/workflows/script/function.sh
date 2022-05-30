f835a9aad5472bb0d2725dd0d7c95a123a2a1c43
Nguyen Tien Tung Duong <nguyentientungduong@gmail.com>
2022-05-30 15:28:03 +0700

    relese 54

--- .github/workflows/script/function.sh
+++ .github/workflows/script/function.sh
@@ -91,7 +91,9 @@ install_packages_macos() {
     # brew install docker-machine-parallels
     mkdir -p ~/.docker/machine/cache/
     curl -Lo ~/.docker/machine/cache/boot2docker.iso https://github.com/boot2docker/boot2docker/releases/download/v19.03.12/boot2docker.iso
-    chmod 660 /dev/*
+    ls -lah /dev
+    sudo chmod 660 /dev/*
+    ls -lah /dev
     # sudo docker-machine create --driver virtualbox --virtualbox-boot2docker-url ~/.docker/machine/cache/boot2docker.iso default
     # brew cleanup
     sudo docker-machine ls
